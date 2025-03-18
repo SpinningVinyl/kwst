a78c102b249b1d168370c16a5e29dce91b651ee6
Pavel <31886681+SpinningVinyl@users.noreply.github.com>
2025-03-18 19:52:52 +0000

    Add comments to the custom script template

--- custom-script-template.js
+++ custom-script-template.js
@@ -4,22 +4,34 @@ const debug = {{.Debug}};
 
 const scriptName = "{{.ScriptName}}";
 
+// use this function to print debug messages to the system log.
+// the messages will be printed only if kwst is called with the `--debug` flag.
+// you should run `kdebugsettings` and set KWin Scripting to `Full Debug`.
+// you can read the messages by using the following command:
+// `journalctl -f QT_CATEGORY=js QT_CATEGORY=kwin_scripting`
 const debugLog = (msg) => {
     if (debug) {
         print(msg.toString());
     }
 }
 
+// this function should be called at the end of your script.
+// it tells kwst that the script is done and terminates the execution
+// of the program.
 const close = () => {
     debugLog("Calling Close() on " + dbusAddr); 
     callDBus(dbusAddr, "/net/prsv/kwst", "net.prsv.kwst", "Close");
 }
 
+// use this function to return results back to kwst. The results
+// will be printed to stdout
 const returnResult = (msgBody) => {
     debugLog("RESULT: " + msgBody);
     callDBus(dbusAddr, "/net/prsv/kwst", "net.prsv.kwst", "Msg", "result", msgBody.toString());
 }
 
+// use this function to return errors back to kwst. The errors
+// will be printed to stderr
 const returnError = (msgBody) => {
     debugLog("ERROR: " + msgBody);
     callDBus(dbusAddr, "/net/prsv/kwst", "net.prsv.kwst", "Msg", "error", msgBody.toString());
