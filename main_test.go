7d362c9ffdbe1f49899eedfbf841a9c74a2651bc
Pavel <31886681+SpinningVinyl@users.noreply.github.com>
2026-08-01 10:51:37 +0100

    set-window-geometry-relative: add to the test table

--- main_test.go
+++ main_test.go
@@ -106,6 +106,24 @@ func TestCommandRunMethods(t *testing.T) {
 			wantParams:   ScriptParams{Uuid: "window-id", X: 10, Y: 20, Width: 640, Height: 480},
 		},
 		{
+			name: "set window geometry relative",
+			command: &SetWindowGeometryRelativeCmd{
+				Uuid:           "window-id",
+				RelativeX:      10.5,
+				RelativeY:      20.25,
+				RelativeWidth:  66.7,
+				RelativeHeight: 50,
+			},
+			wantTemplate: initialTemplate + JS_SET_WINDOW_GEOMETRY_RELATIVE,
+			wantParams: ScriptParams{
+				Uuid:           "window-id",
+				RelativeX:      10.5,
+				RelativeY:      20.25,
+				RelativeWidth:  66.7,
+				RelativeHeight: 50,
+			},
+		},
+		{
 			name:         "set window workspace",
 			command:      &SetWindowWorkspaceCmd{Uuid: "window-id", WorkspaceId: 3},
 			wantTemplate: initialTemplate + JS_SET_WINDOW_WORKSPACE,
